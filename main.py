5b8bcea40e8f324ce321d08b2039b595a5fed7df
EC2 Default User <ec2-user@ip-172-31-78-146.ec2.internal>
2025-12-13 03:01:13 +0000

    Initial commit: FastAPI + Docker + NGINX reverse proxy

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"status": "ok", "message": "Hello from FastAPI in Docker on EC2"}
+
+@app.get("/api/status")
+def status():
+	return {"status": "ok"}
